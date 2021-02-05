f4f479d1e34e14fc11e695cc25b1b943c89ba665
Daham2000 <dhmakla2000@gmail.com>
2021-02-05 15:51:10 +0530

    init

--- lib/screens/posts.dart
+++ lib/screens/posts.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:animated_widgets/animated_widgets.dart';
+import 'package:badges/badges.dart';
 
 class PostState extends StatefulWidget {
   @override
@@ -8,24 +9,36 @@ class PostState extends StatefulWidget {
 
 class _PostState extends State<PostState> {
 
+  var items = List<String>.generate(100, (index) => 'Item $index');
+
   @override
   Widget build(BuildContext context) {
+    int counter = 3;
+
     return Scaffold(
 
       appBar: AppBar(
         title: Text("Dear diary posts"),
         backgroundColor: Colors.blueAccent,
 
-        leading: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            IconButton(
-              icon: Icon(Icons.notifications),
-              onPressed: (){
-              },
+        actions: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(right: 15.0),
+            child: Badge(
+              badgeContent: Text('$counter'),
+              position: BadgePosition.topEnd(top: 5.0),
+              child: Icon(
+                  Icons.notifications,
+                size: 30.0,
+              ),
             ),
-          ],
-        ),
+          ),
+          IconButton(
+            padding: const EdgeInsets.only(right: 15.0),
+            icon: const Icon(Icons.person_rounded, color: Colors.white,),
+          )
+
+        ],
 
       ),
 
